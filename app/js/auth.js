10a1519d396573b13c327d0b75002b5c8ea11941
Your Name01~ <you@example.com>
2024-02-12 15:10:13 -0800

    update

--- app/js/auth.js
+++ app/js/auth.js
@@ -17,15 +17,30 @@ tClickz.addEventListener("click", function () {
   // Perform actions you want when the element is clicked
 });
 
-function submitandgetref() {
+async function submitandgetref() {
+ var checkCatcha = await window.hcaptcha.getResponse();
+  var wits = 'ES_e089dd5c0e444d8aa903dbf882fb2ba1'
+  const url = 'https://api.hcaptcha.com/siteverify';
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `response=${checkCatcha}&secret=${wits}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the hCaptcha verification response
+      console.log(data);
+    });
+
   if (xClickedz && tClickedz) {
     alert(refUrlx.value);
-    
-        
   } else {
     document.getElementById("swal2-title").innerHTML = "Task Error!";
     document.getElementsByClassName("swal2-container")[0].style.display =
       "flex";
-      console.log(refUrlx.value)
+    console.log(refUrlx.value);
   }
 }
