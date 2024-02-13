0f73d4cca946ddc060acbc7e766c830c48accdd6
Your Name01~ <you@example.com>
2024-02-13 07:04:52 -0800

    update

--- app/js/auth.js
+++ app/js/auth.js
@@ -5,6 +5,12 @@ let refUrlx = document.getElementById("refurl");
 var xClickedz = false;
 var tClickedz = false;
 
+
+
+
+
+
+
 xClickz.addEventListener("click", function () {
   xClickz.style.background = "#bf900233";
   xClickedz = true;
@@ -18,29 +24,25 @@ tClickz.addEventListener("click", function () {
 });
 
 async function submitandgetref() {
- var checkCatcha = await window.hcaptcha.getResponse();
-  var wits = 'ES_e089dd5c0e444d8aa903dbf882fb2ba1'
-  const url = 'https://api.hcaptcha.com/siteverify';
 
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: `response=${checkCatcha}&secret=${wits}`,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Handle the hCaptcha verification response
-      console.log(data);
-    });
+ var checkCatcha = await window.hcaptcha.getResponse()
 
+
+  
   if (xClickedz && tClickedz) {
     alert(refUrlx.value);
+    
+        
   } else {
     document.getElementById("swal2-title").innerHTML = "Task Error!";
     document.getElementsByClassName("swal2-container")[0].style.display =
       "flex";
-    console.log(refUrlx.value);
+      console.log(refUrlx.value)
+  }
+
+  if(checkCatcha !== ""){
+    alert("Correct!")
+  }else{
+    alert('Do the challenge')
   }
 }
